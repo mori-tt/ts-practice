1c0e65649a91b23bb04f7f197248922afa44982d
T_Mori <morit0301@gmail.com>
2024-08-13 18:47:00 +0900

    💄 Modules in TypeScript

--- src/7.4.3/index.ts
+++ src/7.4.3/index.ts
@@ -18,17 +18,17 @@
 
 // console.log(fileData);
 
-import { readFileSync } from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+// import { readFileSync } from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const filePath = fileURLToPath(import.meta.url);
-const fileDir = path.dirname(filePath);
-// // プロジェクトのルートディレクトリを取得
-const projectRoot = path.join(fileDir, "..", "..");
-// // uhyo.textファイルのパスを指定
-const dataFile = path.join(projectRoot, "src", "7.4.1", "uhyo.text");
+// const filePath = fileURLToPath(import.meta.url);
+// const fileDir = path.dirname(filePath);
+// // // プロジェクトのルートディレクトリを取得
+// const projectRoot = path.join(fileDir, "..", "..");
+// // // uhyo.textファイルのパスを指定
+// const dataFile = path.join(projectRoot, "src", "7.4.1", "uhyo.text");
 
-const data = readFileSync(dataFile, { encoding: "utf8" });
+// const data = readFileSync(dataFile, { encoding: "utf8" });
 
-console.log(data);
+// console.log(data);
